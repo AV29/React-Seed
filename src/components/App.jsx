5538f99586736aa016b617c61a977a441b8db0b1
AV29 <snumber29@gmail.com>
2019-02-14 13:05:41 +0300

    better version of slider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import NestedComponent from './nested-component/NestedComponent';
 import styled, { ThemeProvider } from 'styled-components';
 import HookExample from "./hooks/HooksSample";
@@ -27,22 +27,65 @@ export const AppWrapper = styled.div`
 `;
 
 class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
 
     this.handleChangeSliderValue = this.handleChangeSliderValue.bind(this);
+    this.handleSequencedClick = this.handleSequencedClick.bind(this);
+    this.handleSporadicClick = this.handleSporadicClick.bind(this);
+    this.handleBatchedClick = this.handleBatchedClick.bind(this);
 
     this.state = {
-      sliderValue: 2
+      sliderValue: 2,
+      counterBatched: 0,
+      counterSequenced: 0,
+      counterSporadic: 0
     }
   }
 
-  handleChangeSliderValue(sliderValue) {
+  handleChangeSliderValue (sliderValue) {
     console.log(sliderValue);
     this.setState({ sliderValue });
   }
 
-  render() {
+  handleBatchedClick () {
+    for (let i = 0; i < 5; i += 1) {
+      this.setState(({ counterBatched }) => ({ counterBatched: counterBatched + 1 }));
+    }
+  }
+
+  handleSporadicClick () {
+    // for (let i = 0; i < 5; i += 1) {
+    //   console.log('qw');
+    //   this.setState({ counterSporadic: this.state.counterSporadic + 1 });
+    // }
+    this.setState({ counterSporadic: this.state.counterSporadic + 1 });
+    this.setState({ counterSporadic: this.state.counterSporadic + 1 });
+    this.setState({ counterSporadic: this.state.counterSporadic + 1 });
+    this.setState({ counterSporadic: this.state.counterSporadic + 1 });
+    this.setState({ counterSporadic: this.state.counterSporadic + 1 });
+  }
+
+  handleSequencedClick () {
+    //this.makeSequencedUpdates(5, () => ({ counterSequenced: this.state.counterSequenced + 1 }));
+    this.makeSequencedUpdates(5);
+  }
+
+  makeSequencedUpdates (count, updater) {
+    let counter = 0;
+
+    const func = () => {
+      counter += 1;
+      if (counter > count) return;
+      //this.setState(updater(), func);
+      this.setState({ counterSequenced: this.state.counterSequenced + 1 }, func);
+    };
+
+    func.bind(this)();
+  }
+
+
+  render () {
     return (
       <ThemeProvider theme={mainTheme}>
         <AppWrapper style={{ width: '50%' }}>
@@ -50,22 +93,50 @@ class App extends Component {
           <Title theme={secondaryTheme}>Hello World!!!</Title>
           <NestedComponent passedProp={'Some passed prop'}/>
           <HookExample/>*/}
+          {/*<button onClick={this.handleBatchedClick}>Batched = {this.state.counterBatched}</button>
+          <button onClick={this.handleSequencedClick}>Sequenced = {this.state.counterSequenced}</button>
+          <button onClick={this.handleSporadicClick}>Sporadic = {this.state.counterSporadic}</button>*/}
           <SliderControl
             label="Slider Example"
             value={this.state.sliderValue}
             onChange={this.handleChangeSliderValue}
-            steps={[
-              { value: 10, label: 'Fast', tooltip: 'Fastest, Test Feasibility' },
-              { value: 223, tooltip: 'Fast, Basic Search' },
-              { value: 312, label: 'Balanced', tooltip: 'Default' },
-              { value: 43, tooltip: 'Slower, Expanded Search' },
-              { value: 56, label: 'Detailed', tooltip: 'Slowest, Advanced Search' }
-            ]}
+            min={1}
+            max={8}
+            step={2}
+            info={{
+              1: { tickMark: 3},
+              2: { tooltip: 'Fast, Basic Search' },
+              3: { tickMark: 'Balanced'},
+              4: { tooltip: 'Slower, Expanded Search' },
+              5: { tooltip: 'Wow', tickMark: 'Detailed'}
+            }}
           />
+          {/*<Parent/>*/}
         </AppWrapper>
       </ThemeProvider>
     );
   }
+}
+
+function Parent () {
+  let [count, setCount] = useState(0);
+  return (
+    <h1 onClick={() => setCount(count + 1)}>
+      Parent clicked {count} times
+      <Child/>
+    </h1>
+  );
+}
+
+function Child () {
+  let [count, setCount] = useState(0);
+  return (
+    <button onClick={() => {
+      setCount(count + 1);
+    }}>
+      Child clicked {count} times
+    </button>
+  );
 }
 
 export default App;
